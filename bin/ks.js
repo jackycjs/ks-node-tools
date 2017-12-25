ab3bbe696fc6e9517f664942fe6d89d3ec401345
Jacky Chen <jacky_cjs@163.com>
2017-12-25 16:34:55 +0800

    add wrapFile

--- bin/ks.js
+++ bin/ks.js
@@ -1,13 +1,15 @@
 #!/usr/bin/env node
 
 var program = require('commander');
-// var filr = require('../lib/file.js');
+var file = require('../lib/file.js');
 
 program
   .command('hello')
   .option("-w, --word [mode]")
+  .option("-p, --path [mode]")
   .action(function(options){
-    console.log('hello', options.word);
+    // console.log('hello', options.word);
+    file.wrapFile(options.path);
   })
 
   
