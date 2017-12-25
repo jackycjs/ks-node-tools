cf00ed0242315c4389d8c4f63226cb943076443d
Jacky Chen <jacky_cjs@163.com>
2017-12-25 17:42:50 +0800

    add flat & fix transcode

--- bin/ks.js
+++ bin/ks.js
@@ -4,13 +4,26 @@ var program = require('commander');
 var file = require('../lib/file.js');
 
 program
-  .command('hello')
-  .option("-w, --word [mode]")
+  .command('wrap')
   .option("-p, --path [mode]")
   .action(function(options){
-    // console.log('hello', options.word);
     file.wrapFile(options.path);
   })
 
-  
+program
+  .command('flat')
+  .option("-p, --path [mode]")
+  .action(function(options){
+    file.flatFolder(options.path);
+  })
+
+program
+  .command('transcode')
+  .option("-p, --path [mode]")
+  .option("-f, --fromCoding [mode]")
+  .option("-t, --toCoding [mode]")
+  .action(function(options){
+    file.transCode(options.path, options.fromCoding, options.toCoding);
+  })
+
 program.parse(process.argv);
